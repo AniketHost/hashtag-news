03d4ad23037ae942e7adc48f54d6910767b84f4a
aniket gurav <aniketgurav2442@gmail.com>
2025-03-02 02:36:16 +0530

    Jobs category

--- controller/loginController.js
+++ controller/loginController.js
@@ -2,7 +2,11 @@ const Login = require('../model/loginModel');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 const jwtSecretKey = crypto.randomBytes(32).toString('hex');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
+
+
+
 const express = require('express');
 
 const app = express();
@@ -23,8 +27,9 @@ exports.loginUser = async (req, res) => {
       return res.status(401).json({ error: 'User not found' });
     }
 
-    const passwordMatch = await bcrypt.compare(password, user.password);
+    const passwordMatch = bcrypt.compareSync(password, user.password);
 
+    console.log("--passmatch---", passwordMatch)
     if (!passwordMatch) {
       return res.status(401).json({ error: 'Authentication failed' });
     }
@@ -49,7 +54,9 @@ exports.registerUser = async (req, res) => {
     const existingUsers = await Login.find();
 
     if (existingUsers.length === 0) {
+      // const hashedPassword = await bcrypt.hashSync(password, 10);
       const hashedPassword = await bcrypt.hash(password, 10);
+
 
       const newUser = new Login({
         userId,
@@ -62,8 +69,8 @@ exports.registerUser = async (req, res) => {
 
       res.status(201).json({ message: 'Primary user registered successfully' });
     }
-    else{
-      return res.status(400).json({message:"Primary user already exist"})
+    else {
+      return res.status(400).json({ message: "Primary user already exist" })
     }
     // } else {
     //   const existingUser = await Login.findOne({ userId });
@@ -72,7 +79,7 @@ exports.registerUser = async (req, res) => {
     //     return res.status(400).json({ error: 'User already exists' });
     //   }
 
-      
+
     //   const hashedPassword = await bcrypt.hash(password, 10);
 
     //   const newUser = new Login({
@@ -85,7 +92,7 @@ exports.registerUser = async (req, res) => {
 
     //   res.status(201).json({ message: 'User registered successfully' });
     // }
-    
+
   } catch (error) {
     console.error('Error during user registration:', error);
     res.status(500).json({ error: 'Internal server error' });
@@ -93,32 +100,32 @@ exports.registerUser = async (req, res) => {
 };
 
 
-  // exports.registerUser = async (req, res) => {
-  //   const { userId, password, email } = req.body;
-  
-  //   try {
-  //     const existingUser = await Login.findOne({ userId });
-  
-  //     if (existingUser) {
-  //       return res.status(400).json({ error: 'User already exists' });
-  //     }
-  
-  //     const hashedPassword = await bcrypt.hash(password, 10); 
-  
-  //     const newUser = new Login({
-  //       userId,
-  //       password: hashedPassword, 
-  //       email
-  //     });
-  
-  //     await newUser.save();
-  
-  //     res.status(201).json({ message: 'User registered successfully' });
-  //   } catch (error) {
-  //     console.error('Error during user registration:', error);
-  //     res.status(500).json({ error: 'Internal server error' });
-  //   }
-  // };
+// exports.registerUser = async (req, res) => {
+//   const { userId, password, email } = req.body;
+
+//   try {
+//     const existingUser = await Login.findOne({ userId });
+
+//     if (existingUser) {
+//       return res.status(400).json({ error: 'User already exists' });
+//     }
+
+//     const hashedPassword = await bcrypt.hash(password, 10); 
+
+//     const newUser = new Login({
+//       userId,
+//       password: hashedPassword, 
+//       email
+//     });
+
+//     await newUser.save();
+
+//     res.status(201).json({ message: 'User registered successfully' });
+//   } catch (error) {
+//     console.error('Error during user registration:', error);
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// };
 
 
 const nodemailer = require('nodemailer');
@@ -129,11 +136,12 @@ function generateOTP() {
 }
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail', 
+  service: 'gmail',
   auth: {
-    user: 'aniketgurav2442@gmail.com', 
-    pass: 'eovdbbyycycctddb'
-
+    // user: process.env.EMAIL_USER, 
+    // pass: process.env.EMAIL_PASS 
+    user : 'aniketgurav2442@gmail.com',
+    pass : 'eovdbbyycycctddb'
   },
 });
 
@@ -159,45 +167,46 @@ exports.verifyOTP = async (req, res) => {
   const { userId } = req.body;
   try {
     const isPrimary = true
-    // console.log("reqqqqqqqqqqqqqq",isPrimary);
+    console.log("reqqqqqqqqqqqqqq", isPrimary);
     const user = await Login.findOne({ userId }).exec();
 
     if (!user) {
       return res.status(401).json({ error: 'User not found' });
     }
 
-    if (req.body.enteredOTP === user.otp  || req.body.enteredOTP === "123") {
+    if (req.body.enteredOTP === user.otp || req.body.enteredOTP === "123") {
 
-      const token = jwt.sign({ userId: user.userId}, jwtSecretKey, { expiresIn: '10000' });
+      const token = jwt.sign({ userId: user.userId }, jwtSecretKey, { expiresIn: '10000' });
       const tokendata = { userId: user.userId, isPrimary: isPrimary };
-      
-      // console.log("ISSSSSS primary",isPrimary);
+
+      console.log("ISSSSSS primary", isPrimary);
       const cookiesValidation = {
         httpOnly: true,
-        expires : new Date(Date.now() + 5 * 60 * 60 * 1000)
+        expires: new Date(Date.now() + 5 * 60 * 60 * 1000)
       }
 
       // console.log(cookiesValidation);
       // res.cookie('accessToken', tokendata, { httpOnly: true, expires: new Date(Date.now() + 5 * 60 * 1000) });
 
-      res.cookie('refreshToken',token,cookiesValidation)
-  
+      res.cookie('refreshToken', token, cookiesValidation)
+
       res.status(200).json({ success: true, token, message: 'OTP verification successful' });
 
 
     }
     else {
-      // console.log(req.body.enteredOTP,"jhdfdfu",user.otp);
+      console.log(req.body.enteredOTP, "jhdfdfu", user.otp);
       return res.status(401).json({ error: 'Incorrect OTP' });
     }
-   
 
-    
+
+
   } catch (error) {
     console.error('Error during OTP verification:', error);
     res.status(500).json({ error: 'Internal server error' });
   }
 };
+
 
 
 
