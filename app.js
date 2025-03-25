60c17dee46c57be8f3a3488984a93a1992547a97
aniket gurav <aniketgurav2442@gmail.com>
2025-03-26 00:54:38 +0530

    Admin-changes

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const cookieParser = require('cookie-parser');
 const axios = require('axios');
 
 const cron = require('node-cron');
+const { error } = require('console');
 
 
 
@@ -22,15 +23,29 @@ dotenv.config();
 const app = express();
 
 const serverUrl = process.env.CLIENT_ORIGIN;
+const interval = 30000;
 
-cron.schedule('*/5 * * * *', async () => {
-  try {
-    const response = await axios.get(serverUrl);
-    console.log(`Health check response: ${response.status} , ${serverUrl}`);
-  } catch (error) {
-    console.error(`Health check error: ${error.message}`);
-  }
-});
+
+function reloadWebsite() {
+
+  axios.get(serverUrl).then((response) => {
+    console.log("##### Website reloaded #######")
+  }).catch((error)=>{
+    console.log(`error : ${error.message}`)
+  })
+}
+
+setInterval(reloadWebsite,interval)
+
+
+// cron.schedule('*/5 * * * *', async () => {
+//   try {
+//     const response = await axios.get(serverUrl);
+//     console.log(`Health check response: ${response.status} , ${serverUrl}`);
+//   } catch (error) {
+//     console.error(`Health check error: ${error.message}`);
+//   }
+// });
 
 
 
